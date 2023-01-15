c748c6668143df12b3516d30e71849554f7bd901
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-01-15 00:22:14 -0300

    Adição do baseURL do Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081',
+    // baseURL: 'http://localhost:8081',
+    baseURL: 'https://gugol-recados-api.vercel.app',
     headers: {
         "Content-type": "application/json"
     },
